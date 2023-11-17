2eab9a0fafdc3d4dbde7820e9e428f3f4b9cbf07
haekyu.cho <haekyu.cho@nhn.com>
2023-11-17 17:31:47 +0900

    second commit

--- src/test/kotlin/com/nhncommerce/clientside/config/ClientSideConfig.kt
+++ src/test/kotlin/com/nhncommerce/clientside/config/ClientSideConfig.kt
@@ -2,38 +2,60 @@ package com.nhncommerce.clientside.config
 
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
+import io.lettuce.core.RedisChannelHandler
 import io.lettuce.core.RedisClient
+import io.lettuce.core.RedisConnectionStateListener
 import io.lettuce.core.TrackingArgs
 import io.lettuce.core.support.caching.CacheAccessor
+import io.lettuce.core.support.caching.CacheFrontend
 import io.lettuce.core.support.caching.ClientSideCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate
+import java.net.SocketAddress
 import java.time.Duration
-import java.util.HashMap
-import java.util.concurrent.ConcurrentHashMap
-import java.util.function.Function
 
 @Configuration
 class ClientSideConfig {
 
     @Bean
-    fun localCache(): Cache<String, String> {
-        return
-    }
+    fun localCacheMap() = CaffeineCacheMap()
 
     @Bean
-    fun clientSideCaching(connectionFactory: LettuceConnectionFactory, localCache: Cache<String, String>) {
+    fun clientSideCaching(
+        connectionFactory: LettuceConnectionFactory,
+        localCacheMap: CaffeineCacheMap,
+    ): CacheFrontend<String, String>? {
         val redisClient = RedisClient.create(connectionFactory.hostName)
         val connection = redisClient.connect()
 
-        val frontend =
-            ClientSideCaching.enable(CacheAccessor.forMap(localCache), connection, TrackingArgs.Builder.enabled())
+        redisClient.addListener(object: RedisConnectionStateListener {
+            override fun onRedisConnected(connection: RedisChannelHandler<*, *>?, socketAddress: SocketAddress?) {
+                super.onRedisConnected(connection, socketAddress)
+            }
+
+            override fun onRedisDisconnected(connection: RedisChannelHandler<*, *>?) {
+                super.onRedisDisconnected(connection)
+            }
+
+            override fun onRedisExceptionCaught(connection: RedisChannelHandler<*, *>?, cause: Throwable?) {
+                super.onRedisExceptionCaught(connection, cause)
+            }
+        })
+
+
+        val cacheFrontend = ClientSideCaching.enable(localCacheMap, connection, TrackingArgs.Builder.enabled())
+        return enable
+    }
+
+    @Bean
+    fun redisTemplate(connectionFactory: LettuceConnectionFactory): ReactiveStringRedisTemplate {
+        return ReactiveStringRedisTemplate(connectionFactory)
     }
 }
 
-
-class CaffeineCacheMap: CacheAccessor<String, String>() {
+class CaffeineCacheMap : CacheAccessor<String, String> {
     private val cache: Cache<String, String> = Caffeine.newBuilder()
         .expireAfterWrite(Duration.ofMillis(1000 * 60 * 60))
         .maximumSize(100_000)
@@ -43,12 +65,11 @@ class CaffeineCacheMap: CacheAccessor<String, String>() {
         return cache.getIfPresent(key)
     }
 
-    override fun put(key: String, value: String): String? {
+    override fun put(key: String, value: String) {
         return cache.put(key, value)
     }
 
-
     override fun evict(key: String?) {
-        TODO("Not yet implemented")
+        return cache.invalidate(key)
     }
 }
